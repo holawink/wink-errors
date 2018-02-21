f66169378e539af91a377511d691cae3cd84f151
rbaronwink <robert@holawink.com>
2018-02-21 17:12:39 -0600

    Add cards error

--- src/cards.js
+++ src/cards.js
@@ -2,6 +2,10 @@ const errors = {
   cardNotBelong: {
     code: 'cards-1',
     message: 'The card does not belong to the customer'
+  },
+  UNABLE_TO_ACTIVATE_NAE_ERROR: {
+    code: 'cards-2',
+    message: 'Nae did not generate the transaction id for the activation'
   }
 }
 

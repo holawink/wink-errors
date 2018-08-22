65002db6e841f1823dfbcdfd4cbb6a9e8c9874ee
rbaronwink <robert@holawink.com>
2018-08-21 23:39:42 -0600

    Add CANNOT_UPDATE_SUBSCRIPTION_TO_MAILCHIMP

--- src/email.js
+++ src/email.js
@@ -2,6 +2,10 @@ const errors = {
   CANNOT_SUBSCRIBE_TO_MAILCHIMP: {
     code: 'email-1',
     message: 'Error on Register on MailChimp'
+  },
+  CANNOT_UPDATE_SUBSCRIPTION_TO_MAILCHIMP: {
+    code: 'email-2',
+    message: 'Error on update on MailChimp'
   }
 }
 

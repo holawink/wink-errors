83c5566f38f119809973cd3a0f50fc1d6c0429c8
rbaronwink <robert@holawink.com>
2019-01-21 10:09:03 -0600

    Add ticket not found for PP

--- src/paypal.js
+++ src/paypal.js
@@ -2,6 +2,10 @@ const errors = {
   AMOUNT_TOO_LOW: {
     code: 'paypal-1',
     message: 'Amount must be greater than fees'
+  },
+  TICKET_NOT_FOUND: {
+    code: 'paypal-2',
+    message: 'Ticket not found'
   }
 }
 

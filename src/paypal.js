0af2d8ca3b3b760405fd8b92cc9a8b4b208bcabd
AlkinsonGonzalez <44472608+AlkinsonGonzalez@users.noreply.github.com>
2019-05-24 09:25:02 -0600

    WMA-1964-PayPal-WithDrawalErrors (#10)

--- src/paypal.js
+++ src/paypal.js
@@ -18,7 +18,24 @@ const errors = {
   INVALID_PAYPAL_ACCOUNT_NAME: {
     code: 'paypal-5',
     message: 'Your id name does not match with paypal account name'
+  },
+  VALIDATION_ERROR: {
+    code: 'paypal-6',
+    message: 'Currency is not allowed for this transaction'
+  },
+  PAYMENT_DENIED: {
+    code: 'paypal-7',
+    message: 'Payment was Denied'
+  },
+  UNAUTHORIZED: {
+    code: 'paypal-8',
+    message: 'Request is Unauthorized'
+  },
+  INTERNAL_SERVICE_ERROR: {
+    code: 'paypal-9',
+    message: 'PayPal internal error'
   }
+
 }
 
 export default errors

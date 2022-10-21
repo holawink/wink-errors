aa3dbecd02f633b4f36b1ade8e40d2cf83d91619
Lucía Zamora <lucia.zamorah@gmail.com>
2022-10-21 16:22:44 +0200

    Task/PD-1040: Add invalid Account code (#47)
    * PD-1040: Add invalid account code
    
    * PD-1040: fix typo
    
    * PD-1040: fix typo

--- dist/sendMoney.js
+++ dist/sendMoney.js
@@ -31,6 +31,10 @@ const errors = {
   FAILED_TRANSACTION_TO_EXTERNAL_BANK: {
     code: 'sendMoney-7',
     message: 'It was not possible to register the transaction to the account of the client because a problem occurred'
+  },
+  INVALID_ACCOUNT_NUMBER: {
+    code: 'sendMoney-8',
+    message: 'Invalid account number'
   }
 };
 

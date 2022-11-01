8c1c2d755b04fc53c67590584ac5330a69d1f6c6
Yoher <yoher.od96@hotmail.com>
2022-11-01 10:47:09 -0600

    PD-1136: Add new error code for service payment

--- dist/servicePayments.js
+++ dist/servicePayments.js
@@ -12,6 +12,10 @@ const errors = {
   PAYMENTS_BLOCKED: {
     code: 'service-payments-0-2',
     message: 'You are not allowed to pay services'
+  },
+  RECHARGE_PHONE_NUMBER: {
+    code: 'service-payments-0-3',
+    message: 'An error occurred while recharging the number'
   }
 };
 

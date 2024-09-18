79fb2965a86c32868430435d9bae25968ae3b225
Jose Andres Araya <jose.araya@holawink.com>
2024-09-18 10:59:25 -0600

    PDL-6657: Adding errors from the mapping done between the SINPE standard and what the ODB returns

--- src/reloadMoney.js
+++ src/reloadMoney.js
@@ -5,7 +5,7 @@ const errors = {
   },
   LIMIT_EXCEEDED: {
     code: 'reload-2',
-    message: 'Monthy limit exceeded'
+    message: 'Monthly limit exceeded'
   },
   REDUCE_AMOUNT_LIMIT_EXCEEDED: {
     code: 'reload-3',
@@ -17,7 +17,75 @@ const errors = {
   },
   DESTINATION_DONT_ALLOW_THE_TRX: {
     code: 'reload-5',
-    message: 'THE DESTINATION DONT ALLOW THE TRANSACCTION'
+    message: 'The destination did not allow the transaction'
+  },
+  DESTINATION_INCORRECT_RESPONSE: {
+    code: 'reload-6',
+    message: 'The destination response is not correct'
+  },
+  CLIENT_ACCOUNT_CLOSED: {
+    code: 'reload-7',
+    message: 'Client account is closed'
+  },
+  CLIENT_ACCOUNT_INSUFFICIENT_FUNDS: {
+    code: 'reload-8',
+    message: 'Client account does not have enough funds'
+  },
+  CLIENT_ACCOUNT_NOT_FOUND: {
+    code: 'reload-9',
+    message: 'Client account does not exist' 
+  },
+  CLIENT_ACCOUNT_BLOCKED: {
+    code: 'reload-10',
+    message: 'Client account is blocked' 
+  },
+  IDENTITY_DOES_NOT_MATCH: {
+    code: 'reload-11',
+    message: 'Identity does not match with destination' 
+  },
+  CLIENT_ACCOUNT_INCORRECT_NUMBER: {
+    code: 'reload-12',
+    message: 'Client account number is incorrect'
+  },
+  DESTINATION_INSUFFICIENT_FUNDS: {
+    code: 'reload-13',
+    message: 'Destination does not have enough funds'
+  },
+  CURRENCY_DOES_NOT_MATCH: {
+    code: 'reload-14',
+    message: 'Currency does not match'
+  },
+  DESTINATION_BLOCKED_TRANSACTION: {
+    code: 'reload-15',
+    message: 'Destination did not authorize the transaction'
+  },
+  CLIENT_NOT_AUTHORIZED: {
+    code: 'reload-16',
+    message: 'Origin client is not authorized'
+  },
+  SERVICE_NOT_AUTHORIZED: {
+    code: 'reload-17',
+    message: 'Service is not authorized'
+  },
+  DESTINATION_NOT_AVAILABLE: {
+    code: 'reload-18',
+    message: 'Destination could not process the transaction'
+  },
+  DESTINATION_TIMEOUT: {
+    code: 'reload-19',
+    message: 'Response time exceeded by destination'
+  },
+  DESTINATION_EXCEPTION: {
+    code: 'reload-20',
+    message: 'Exception received by destination'
+  },
+  DESTINATION_MISCOMMUNICATION: {
+    code: 'reload-20',
+    message: 'Communication error with destination'
+  },
+  PROCESSING_EXCEPTION: {
+    code: 'reload-21',
+    message: 'SINPE failed to process the transaction'
   }
 }
 

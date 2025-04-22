92dcb290410fb583d92bb5a63be1a7962cba90b1
Geovanny Burgos Retana <geoburgosretana@gmail.com>
2025-04-22 09:43:52 -0600

    PDL-9919: Add new login attempt limit error (#81)
    * PDL-9919: Add new login attempt limit error
    
    * PDL-9919: Add new login attempt limit error
    
    * PDL-9919: Add new login attempt limit error

--- src/auth.js
+++ src/auth.js
@@ -163,6 +163,10 @@ const errors = {
   MAX_SEND_ATTEMPTS_REACHED: {
     code: 'security-41',
     message: 'Max send attempts reached'
+  },
+  MAX_LOGIN_ATTEMPS_REACHED: {
+    code: 'security-42',
+    message: 'Maximum number of login attemps reached'
   }
 }
 

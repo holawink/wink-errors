5c758cecf6bed4c8e77c34a6a0fb74556b43a8ad
Geovanny Burgos Retana <geoburgosretana@gmail.com>
2025-05-13 14:06:01 -0600

    PDL-10419: Add error for card link failure after status update

--- src/account.js
+++ src/account.js
@@ -2,6 +2,11 @@ const errors = {
   ALREADY_SAVED: {
     code: 'account-1',
     message: 'Account already saved'
+  },
+  CARD_LINK_FAILED_AFTER_ACCOUNT_STATUS_UPDATE: {
+    code: 'account-2',
+    message:
+      'The account status was successfully updated, but the card could not be linked to the account.'
   }
 }
 
